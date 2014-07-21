3c0fc8e45ed76f4695c0dc8bd319d351b7d71add
plato-cambrian <plato@cambrian.org>
2014-07-21 11:52:31 -0400

    more jasmine WIP

--- js/testHarnessApp.js
+++ js/testHarnessApp.js
@@ -18,74 +18,41 @@ var testHarness = angular.module("testHarnessApp", [])
 
   // Get array of tests (created in js/testsArray.js):
   $scope.tests = Cambrian.japiTests.tests;
-      
-  $scope.runTests = function(){
-    console.log('runTests()');
-    $scope.testsDisabled = true; // "the test suite is complete, don't allow a second run"
+  describe("Mock JAPI calls", function(){
+    angular.forEach($scope.tests, function (test) {
+      describe(test.name, function(){
+        //it('calls promise.done after running asynchronous tests', function(done){
+        //afterEach(function(done){
 
-    describe("Mock JAPI calls", function(){
-    //  angular.forEach($scope.tests, function (test) {
-      $scope.tests.forEach(function(test){
-        describe(test.name, function(){
-          console.log('Outside it');
-          it('matches '+test.expected, function(){
+        //});
 
-            var deferred = $q.defer();
-            var promise = deferred.promise;
+          var deferred = $q.defer();
+          var promise = deferred.promise;
 
-            promise.then(onTestComplete, onTestError);
+          promise.then(onTestComplete, onTestError);
+          //.finally(done);
 
-            function onTestComplete (data) {
-              test.actual = data;
-              test.succeeded = angular.equals(test.actual, test.expected);
-              expect(test.succeeded).toEqual(test.actual);
-              //done();
-            };
+          function onTestComplete (data) {
+            console.log('Outside it completes');
+            describe('The test result', function(){
+              it('matches '+test.expected, function(){
+                console.log('inside it');
+                test.actual = data;
+                expect(test.expected).toEqual(test.actual);
+              }); //end of it()
+            });
+          };
 
-            function onTestError(reason) {
+          function onTestError(reason) {
+            console.log('Outside it errs');
+            it('does not fail', function(){
+              console.log('inside it');
               test.succeeded = false;
               test.error = reason;
               console.log('Fail -',test.name);
               console.log('     Error:', reason)
               expect(test.error).not.toBeDefined();
-              //done();
-            };
-            
-            // Alias the function to be tested as testFn:
-            var testFn = test.functionCall;
-           
-            // Make a callback fn that will eventually resolve this one deferred, for this one test:
-
-            var testCb = (function(deferred){
-              return function(err, resultData){
-                //console.log("Test callback", err, resultData)
-                if(err){  deferred.reject(err);  };
-                deferred.resolve(resultData)
-              }
-            })(deferred);
-           
-            // Append the test callback to the test arguments:
-          var deferred = $q.defer();
-          var promise = deferred.promise;
-
-          promise.then(onTestComplete, onTestError);
-
-          function onTestComplete (data) {
-            test.actual = data;
-            test.succeeded = angular.equals(test.actual, test.expected);
-            var status = test.succeeded ? "PASS" : "FAIL";
-            console.log(status,'-',test.name);
-            if(!test.succeeded){
-              console.log('     Expected:', test.expected);
-              console.log('     Actual:', test.actual);
-            }
-          };
-
-          function onTestError(reason) {
-            test.succeeded = false;
-            test.error = reason;
-            console.log('Fail -',test.name);
-            console.log('     Error:', reason)
+            });
           };
           
           // Alias the function to be tested as testFn:
@@ -115,28 +82,62 @@ var testHarness = angular.module("testHarnessApp", [])
             testCb(e);
           } finally {
           };
+        //}); // end of it()
+      }); // end of describes(one)
+    }); // end of angular.forEach
+  }); // end of describes(all)
+      
+  /*
+  $scope.runTests = function(){
+    console.log('runTests()');
+    $scope.testsDisabled = true; // "the test suite is complete, don't allow a second run"
+    // Test one:
+    describe('The first test', function(){
+      beforeEach(function(done){ done() });
+      it('calls done when the promise finishes', function(done){
+        console.log('inside async it');
+        // Set up a promise
+        var deferred = $q.defer();
+        var promise = deferred.promise;
+        promise.then(onTestComplete)  //, onTestError)
+        .finally(done)
 
-            test.arguments.push(testCb)
-            
-            try {
-              // Run the test:
-              testFn.apply(this, test.arguments)
-              
-              // At some future point, the test will call its callback argument,
-              // which in turn calls deferred.resolve or deferred.reject.
-            } catch(e) {
-              // Manually pass the caught exception to the callback:
-              testCb(e);
-            } finally {
-            };
+        function onTestComplete (data) {
+          console.log('Outside it completes');
+          it('matches '+test.expected, function(done){
+            console.log('inside it');
+            test.actual = data;
+            test.succeeded = angular.equals(test.actual, test.expected);
+            expect(test.succeeded).toEqual(test.actual);
           }); //end of it()
+        };
 
-        }); // end of describes(one)
-      }); // end of angular.forEach
-    }); // end of describes(all)
+        function onTestError(reason) {
+          console.log('Outside it errs');
+          it('does not fail', function(done){
+            console.log('inside it');
+            test.succeeded = false;
+            test.error = reason;
+            console.log('Fail -',test.name);
+            console.log('     Error:', reason)
+            expect(test.error).not.toBeDefined();
+          });
+        };
+
+        // Run the test then trigger the promise
+        var test = $scope.tests[0];
+        var callback = function(err, resultData){
+          if(err){  deferred.reject(err);  };
+          deferred.resolve(resultData)
+        };
+      });
+    });
   }; // end of $scope.runTests definition
+  */
 }) // end of testCtrl
 .controller("childTestCtrl", function($scope, $q){
 
 })
+
+
 
