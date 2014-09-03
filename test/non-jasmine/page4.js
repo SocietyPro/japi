ac3e2e70569a59e841e51d23bab4457c5430db52
Cesar Reyes <creyes@xik.gt>
2014-09-03 16:59:41 -0600

    Data binding test

---
+++ test/non-jasmine/page4.js
@@ -0,0 +1,30 @@
+var myApp = angular.module('myApp',[]);
+
+// root object
+if(Cambrian === undefined || Cambrian.isMockCambrian === true){
+  throw("japi.js needs a root object Cambrian.")
+};
+
+
+var Cambrian = Cambrian || {}
+
+var g_chores = {
+	list: ["laundry", "lunch", "grapes", "satellite"]
+};
+
+var Add = function() {
+	g_chores.list.push("nothing");
+}
+
+myApp.controller('ApiTestController', ['$scope', function($scope) {
+  	$scope.greeting = 'Hola!';
+    $scope.Cambrian = Cambrian;
+    $scope.chores = g_chores;
+
+    $scope.Add = function() {
+    	$scope.chores.list.push("NaNgular " + (new Date())  );
+    }
+
+}]);
+
+
